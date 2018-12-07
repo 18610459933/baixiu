97034e175c255e752ec2d76fbccacbe4a9c2718e
xiao qing <451547047@qq.com>
2018-12-07 18:28:03 +0800

    ajax

--- admin/api/comments.php
+++ admin/api/comments.php
@@ -2,9 +2,31 @@
 //引入封装的函数
 require_once "../../functions.php";
 
-$comments = xiu_fetch_all("select * from comments;");
+$page = empty($_GET['page']) ? 1 : intval($_GET['page']);
 
-$json = json_encode($comments);
+$length = 3;
+$offset = ($page - 1) * $length;
+
+$sql = sprintf("select
+	comments.*,
+	posts.title as post_title
+from comments	
+inner join posts on comments.post_id = posts.id
+order by comments.created desc
+limit %d, %d;", $offset, $length);
+
+$comments = xiu_fetch_all($sql);
+
+$total_count = xiu_fetch_one("select count(1) as count
+from comments
+inner join posts on comments.post_id = posts.id")['count'];
+
+$total_pages = ceil($total_count / $length);
+
+$json = json_encode(array(
+    'comments' => $comments,
+    'total_pages' => $total_pages
+));
 
 header("Content-Type: application/json");
 
--- admin/comments.php
+++ admin/comments.php
@@ -41,16 +41,16 @@ xiu_get_current_user();
           <li><a href="#">下一页</a></li>
         </ul>
       </div>
-      <table class="table table-striped table-bordered table-hover">
+      <table class="table table-striped table-bordered table-hover table-responsive">
         <thead>
           <tr>
             <th class="text-center" width="40"><input type="checkbox"></th>
-            <th>作者</th>
+            <th width="70">作者</th>
             <th>评论</th>
-            <th>评论在</th>
-            <th>提交于</th>
+            <th width="200">评论在</th>
+            <th width="90">提交于</th>
             <th>状态</th>
-            <th class="text-center" width="100">操作</th>
+            <th class="text-center" width="140">操作</th>
           </tr>
         </thead>
         <tbody></tbody>
@@ -61,48 +61,52 @@ xiu_get_current_user();
   <?php $current_page = 'comments'; ?>
   <?php include 'inc/sidebar.php'; ?>
 
-  <script type="text/javascript" src="/static/assets/vendors/jquery/jquery.js"></script>
-  <script type="text/javascript" src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
-  <script type="text/javascript" src="/static/assets/vendors/jsrender/jsrender.js"></script>
   <script id="comments_tmpl" type="text/x-jsrender">
     {{for comments}}
       <tr>
         <td class="text-center"><input type="checkbox" data-id="{{:id}}"></td>
         <td>{{:author}}</td>
         <td>{{:content}}</td>
-        <td>《Hello world》</td>
+        <td>《{{:post_title}}》</td>
         <td>{{:created}}</td>
         <td>{{:status}}</td>
         <td class="text-center">
-          <a href="post-add.php" class="btn btn-info btn-xs">批准</a>
+          {{if status == 'held'}}
+            <a href="post-add.php" class="btn btn-info btn-xs">批准</a>
+            <a href="post-add.php" class="btn btn-warning btn-xs">拒绝</a>
+          {{/if}}
           <a href="javascript:;" class="btn btn-danger btn-xs">删除</a>
         </td>
       </tr>
     {{/for}}
   </script>
+  <script type="text/javascript" src="/static/assets/vendors/jquery/jquery.js"></script>
+  <script type="text/javascript" src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
+  <script type="text/javascript" src="/static/assets/vendors/jsrender/jsrender.js"></script>
+  <script type="text/javascript" src="/static/assets/vendors/twbs-pagination/jquery.twbsPagination.js"></script>
   <script type="text/javascript">
-    $.get("/admin/api/comments.php",{},function (rec) {
-      var html = $("#comments_tmpl").render({
-        comments : rec
+
+    function loadPage (page){
+      $.get("/admin/api/comments.php",{page:page},function (rec) {
+        $('.pagination').twbsPagination({
+          totalPages: rec.total_pages,
+          visiblePages:3,
+          initiateStartPageClick:false,
+          first:'首页',
+          prev:'上一页',
+          next:'下一页',
+          last:'尾页',
+          onPageClick: function (evt, page) {
+            loadPage(page);
+          }
+        });
+//        console.log(rec);
+        var html = $("#comments_tmpl").render({comments : rec.comments});
+        $('tbody').html(html);
       });
-      $('tbody').html(html);
-    })
-  </script>
-  <script type="text/javascript">
-    $(function($){
-      var $input = $('tbody input');
-      var $theadInput = $('thead input');
-      var $btnDelete = $('#btn_delete');
-      var checkedAll = [];
-      $theadInput.on('change',function () {
-        var $checked = $(this).prop('checked');
-        $('tbody input').prop('checked',$checked).change();
-      });
-//      获取选中tbody中的checked
-      $('.table').on('change',function(){
-        console.log($(this).prop("checked"));
-      });
-    });
+    }
+
+    loadPage(1);
   </script>
   <script>NProgress.done()</script>
 </body>
